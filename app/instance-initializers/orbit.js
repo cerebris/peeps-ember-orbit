4a3c20872c6a24999e686fe7d5ca767c1b5c9e26
Dan Gebhardt <dan@cerebris.com>
2016-09-13 18:01:48 -0400

    Re-introduce remote source + start using bucket for sources.

--- app/instance-initializers/orbit.js
+++ app/instance-initializers/orbit.js
@@ -1,8 +1,9 @@
 import Orbit from 'orbit';
 // import Coordinator from 'orbit/coordinator';
 import JSONAPISource from 'orbit-jsonapi/jsonapi-source';
-import LocalStorageSource from 'orbit-local-storage/local-storage-source';
+// import LocalStorageSource from 'orbit-local-storage/local-storage-source';
 import IndexedDBSource from 'orbit-indexeddb/indexeddb-source';
+import IndexedDBBucket from 'orbit-indexeddb/indexeddb-bucket';
 import {
   ClientError,
   NetworkError
@@ -12,48 +13,53 @@ import fetch from 'ember-network/fetch';
 export function initialize(appInstance) {
   Orbit.fetch = fetch;
 
+  let bucket = new IndexedDBBucket({ dbName: 'peeps-bucket' });
+
   let storeService = appInstance.lookup('service:store');
   let store = storeService.orbitStore;
   let schema = store.schema;
   // let backup = new LocalStorageSource({ schema, namespace: 'peeps' });
-  let backup = new IndexedDBSource({ schema, dbName: 'peeps' });
-  let remote = new JSONAPISource({ schema, keyMap: store.keyMap });
+  let backup = new IndexedDBSource({ schema, bucket, dbName: 'peeps' });
+  let remote = new JSONAPISource({ schema, bucket, keyMap: store.keyMap });
 
   appInstance.register('data-source:backup', backup, { instantiate: false });
   appInstance.register('data-source:remote', remote, { instantiate: false });
 
   store.on('transform', transform => console.log(transform));
 
+  // Push update requests to the server
+  store.on('update', transform => {
+    console.log('store updated, will push to remote - transform:', transform, 'queue backlog:', remote.requestQueue.length);
+    remote.push(transform);
+  });
+
+  // Handle server errors
+  remote.on('pushFail', (transform, e) => {
+    console.log('pushFail', transform.id, e);
+
+    if (e instanceof NetworkError) {
+      // When network errors are encountered, try again in 5s
+      console.log('NetworkError - will try again soon - transform:', transform.id);
+      setTimeout(() => {
+        remote.requestQueue.retry();
+      }, 5000);
+    } else if (e instanceof ClientError) {
+      // Roll back client errors
+      if (store.transformLog.contains(transform.id)) {
+        console.log('Rolling back - transform:', transform.id);
+        store.rollback(transform.id, -1);
+        remote.requestQueue.clear();
+      }
+    }
+  });
+
+  // Sync remote changes with the store
+  remote.on('transform', transform => { store.sync(transform); });
+
   // let coordinator = appInstance.lookup('service:coordinator');
   // coordinator.addSource(store);
   // coordinator.addSource(backup);
   // coordinator.addSource(remote);
-
-  // // push update requests to the server
-  // store.on('update', transform => { remote.push(transform); });
-  //
-  // // handle server errors
-  // remote.on('pushFail', (transform, e) => {
-  //   console.log('pushFail', transform.id, e);
-  //
-  //   if (e instanceof NetworkError) {
-  //     // When network errors are encountered, try again in 5s
-  //     setTimeout(() => {
-  //       console.log('NetworkError - trying again', transform.id);
-  //       remote.requestQueue.retry();
-  //     }, 5000);
-  //   } else if (e instanceof ClientError) {
-  //     // Roll back client errors
-  //     if (store.transformLog.contains(transform.id)) {
-  //       console.log('Rolling back', transform.id);
-  //       store.rollback(transform.id, -1);
-  //       remote.requestQueue.clear();
-  //     }
-  //   }
-  // });
-  //
-  // // sync remote changes with the store
-  // remote.on('transform', transform => { store.sync(transform); });
 }
 
 export default {
--- app/routes/application.js
+++ app/routes/application.js
@@ -9,11 +9,11 @@ export default Ember.Route.extend({
     const backup = owner.lookup('data-source:backup');
     const store = owner.lookup('service:store').orbitStore;
 
-    // warm the store's cache from backup
+    // Warm the store's cache from backup
     return backup.pull(qb.records())
-      .then(transforms => store.sync(transforms))
+      .then(transform => store.sync(transform))
       .then(() => {
-        // backup subsequent changes to the store
+        // Backup subsequent changes to the store
         store.on('transform', transform => backup.sync(transform));
       });
   }
