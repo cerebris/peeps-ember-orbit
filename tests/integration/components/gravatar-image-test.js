80a54ad227747753c88393b20c40232c82fb96ac
Dan Gebhardt <dan@cerebris.com>
2018-10-18 18:12:22 -0400

    ember-qunit-codemod

--- tests/integration/components/gravatar-image-test.js
+++ tests/integration/components/gravatar-image-test.js
@@ -1,24 +1,26 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('gravatar-image', 'Integration | Component | gravatar image', {
-  integration: true
-});
+module('Integration | Component | gravatar image', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{gravatar-image}}`);
+    await render(hbs`{{gravatar-image}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#gravatar-image}}
-      template block text
-    {{/gravatar-image}}
-  `);
+    // Template block usage:
+    await render(hbs`
+      {{#gravatar-image}}
+        template block text
+      {{/gravatar-image}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(this.$().text().trim(), 'template block text');
+  });
 });
--- tests/unit/controllers/contact/index-test.js
+++ tests/unit/controllers/contact/index-test.js
@@ -1,12 +1,12 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('controller:contact/index', 'Unit | Controller | contact/index', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-});
+module('Unit | Controller | contact/index', function(hooks) {
+  setupTest(hooks);
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:contact/index');
+    assert.ok(controller);
+  });
 });
